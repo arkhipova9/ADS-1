09c2c564fab125f4c658e22632e31a1e82550c52
arkhipova9 <116936616+arkhipova9@users.noreply.github.com>
2023-02-25 23:24:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,48 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  if (value <= 1) return false; 
+    for (int num = 2; num <= value / 2; num++) {
+        if (value % num == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  int value = 1, num = 0;
+        while (num < n) {
+            value++;
+            if (checkPrime(value)) {
+                num++;
+            }
+        }
+        return value;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  for (int num = 1; ; num++) {
+            if (checkPrime(value + num)) {
+                if ((value - num >= 2) && checkPrime(value + num)) {
+                    return value + num;
+                }
+                else if (value - num == 1) {
+                    return value + num;
+                }
+                else
+                    return value - num;
+                break;  
+            }
+        }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  int sum = 0;
+    for (int num = 2; num < hbound; num++) {
+        if (checkPrime(num)) {
+            sum += num;
+        }
+    }
+    return sum;
 }
