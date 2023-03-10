a01d58892177e25c3dad61acbd30e9fccf6a5eae
arkhipova9 <116936616+arkhipova9@users.noreply.github.com>
2023-03-10 20:27:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,8 +4,8 @@
 
 
 bool checkPrime(uint64_t value) {
-    for (int num=2; num*num<=value; num++) {
-        if (value%num == 0) {
+    for (int num = 2; num * num <= value; num++) {
+        if (value % num == 0) {
             return false;
         }
     }
@@ -13,9 +13,9 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-  uint64_t value=1;
-  uint64_t num=0;
-  while (num!=n) {
+  uint64_t value = 1;
+  uint64_t num = 0;
+  while (num != n) {
     value++;
     if (checkPrime(value) == true) {
       num++;
@@ -25,17 +25,19 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-  do value++;
+  do {
+      value++;
+  }
   while (checkPrime(value) == false);
   return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t sum=0;
-  uint64_t num=1;
-    while (nextPrime(num)<hbound) {
-      sum+=nextPrime(num);
-      num=nextPrime(num);
+  uint64_t sum = 0;
+  uint64_t num = 1;
+    while (nextPrime(num) < hbound) {
+      sum += nextPrime(num);
+      num = nextPrime(num);
     }
     return sum;
 }
